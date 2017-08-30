5175ba6b653bfbdee3322bd614ea77472a1a07ca
Lou George <lou@Lous-MacBook-Pro.local>
2017-08-29 22:46:07 -0700

    Got the needle animating back and forth ok.

--- app/src/main/java/com/glprod/lou/collektr/SplashScreen.java
+++ app/src/main/java/com/glprod/lou/collektr/SplashScreen.java
@@ -13,12 +13,14 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        RotateAnimation rotation = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        rotation.setDuration(10000);
+        RotateAnimation rotation = new RotateAnimation(0.0f, 3, Animation.RELATIVE_TO_SELF, 0.95f, Animation.RELATIVE_TO_SELF, 0.1f);
+        rotation.setDuration(600);
         rotation.setInterpolator(new LinearInterpolator());
+        rotation.setRepeatCount(Animation.INFINITE);
+        rotation.setRepeatMode(Animation.REVERSE);
         rotation.setFillAfter(true);
 
-        ImageView shadows = (ImageView) findViewById(R.id.Shadows);
+        ImageView shadows = (ImageView) findViewById(R.id.Needle);
         shadows.startAnimation(rotation);
     }
 }
